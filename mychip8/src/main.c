1fb520779a4b3042fc6358fde931bbcfc7664f06
Sudeep <sudeep@LAPTOP-94G82CPU.localdomain>
2026-02-15 18:30:40 +0530

    Added fontset

--- mychip8/src/main.c
+++ mychip8/src/main.c
@@ -1,14 +1,27 @@
 #include <stdio.h>
 // #include "../lowlevel/mychip8/include/chip8.h"
 #include "chip8.h"
+void print_font_visual(Chip8 *cpu, int digit)
+{
+    printf("Font for digit %X:\n", digit);
+    for (int i = 0; i < 5; i++)
+    {
+        uint8_t byte = cpu->memory[digit * 5 + i];
+        for (int bit = 7; bit >= 4; bit--)
+        { // Only use top 4 bits
+            printf("%c", (byte & (1 << bit)) ? '#' : '.');
+        }
+        printf("\n");
+    }
+}
 
 int main()
 {
     Chip8 cpu;
     chip8_init(&cpu);
-    // printf("Testing fontset loading: \n");
-    // printf("First character (0) at 0x000\n");
-    // for(int i = 0; i)
+    printf("Testing fontset loading: \n");
+    printf("First character (0) at 0x000\n");
+    // for (int i = 0; i)
 
     if (chip8_load_rom(&cpu, "ibm_logo.ch8"))
     {
